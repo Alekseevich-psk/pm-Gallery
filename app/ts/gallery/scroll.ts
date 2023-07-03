a92b132f2efcd0a41b629b34be284602b8110f42
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-07-03 15:28:57 +0300

    remove scroll.ts

--- app/ts/gallery/scroll.ts
+++
@@ -1,8 +0,0 @@
-export default function eventScroll(wrapper: Element) {
-    const pmGalleryInnerPreviews = wrapper.querySelector('.pm-gallery__inner--previews');
-
-    pmGalleryInnerPreviews.addEventListener('wheel', function (event: WheelEvent) {
-        console.log(event.deltaY);
-        // window.
-    })
-}
