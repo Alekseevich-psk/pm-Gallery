4bf857d53ad294beba827ed0476f1e03251da866
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-07-04 16:11:48 +0300

    add dist

---
+++ dist/style.js
@@ -0,0 +1,1 @@
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.PmGallery=t():e.PmGallery=t()}(self,(()=>(()=>{"use strict";var e={};return e.default})()));
