b56f29bbef4767cadd8d7d08196e82ac9212f746
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-10-04 16:14:53 +0300

    fixed getOptionsBreakpoints

--- app/ts/helpers/get-options-breakpoints.ts
+++ app/ts/helpers/get-options-breakpoints.ts
@@ -2,8 +2,6 @@ function getOptionsBreakpoints(pmGallery: any) {
     const windowInnerWidth = document.documentElement.clientWidth;
     const breakpointOptions = pmGallery.initOptions.breakpoint;
 
-    if (!breakpointOptions) return;
-
     getOptionsOnScreenWidth(windowInnerWidth, breakpointOptions);
 
     function getOptionsOnScreenWidth(screenWidth: number, options: any) {
