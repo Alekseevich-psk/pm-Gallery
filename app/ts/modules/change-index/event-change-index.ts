79ee833e4e9d895931376192fa81d665c95112d8
Alekseevich-psk <ap-pskov@ya.ru>
2023-10-06 15:52:10 +0300

    fixed eventChangeIndex

--- app/ts/modules/change-index/event-change-index.ts
+++ app/ts/modules/change-index/event-change-index.ts
@@ -1,6 +1,3 @@
-import changeActivePreview from "../change-slide/change-active-preview";
-import changeMainPicture from "../change-slide/change-main-picture";
-
 function eventChangeIndex(pmGallery: any, index: number) {
     pmGallery.wrapper.dispatchEvent(new CustomEvent("changeActiveIndex", {
         detail: { activeIndex: index }
