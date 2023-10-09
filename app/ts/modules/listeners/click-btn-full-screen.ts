f4bdfc12682f53427b399df1d40b0bbd7ebabe53
Alekseevich-psk <ap-pskov@ya.ru>
2023-10-09 16:09:57 +0300

    fixed click-btn-full-screen

--- app/ts/modules/listeners/click-btn-full-screen.ts
+++ app/ts/modules/listeners/click-btn-full-screen.ts
@@ -1,21 +1,26 @@
 import pmgClasses from "../../setting/pmgClasses";
 import initPosPreviews from "../../previews/position-previews/init-position-previews";
+import getOptionsBreakpoints from "../../helpers/get-options-breakpoints";
 
 function clickBtnFullScreen(pmGallery: any) {
     const body = document.querySelector('body');
+    const wrapper = pmGallery.wrapper;
+
     let flagFullScreen = false;
 
     pmGallery.btnScreen.addEventListener('click', () => {
 
         (!flagFullScreen) ? flagFullScreen = true : flagFullScreen = false;
 
-        pmGallery.wrapper.classList.toggle(pmgClasses['fullScreen']);
+        wrapper.classList.toggle(pmgClasses['fullScreen']);
         body.classList.toggle(pmgClasses['overflow']);
 
         pmGallery.fullScreen = flagFullScreen;
+
+        getOptionsBreakpoints(pmGallery);
         initPosPreviews(pmGallery);
 
-        pmGallery.wrapper.dispatchEvent(new CustomEvent("fullScreen", {
+        wrapper.dispatchEvent(new CustomEvent("fullScreen", {
             detail: { fullScreen: flagFullScreen }
         }));
     });
