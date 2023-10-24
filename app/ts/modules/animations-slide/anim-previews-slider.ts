45ae4318a3add4b4cf0aa3a5f3ac0f2ed7bf2bfb
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-10-24 11:11:00 +0300

    fixed fullcreen mode

---
+++ app/ts/modules/animations-slide/anim-previews-slider.ts
@@ -0,0 +1,5 @@
+function animPreviewsSlider() {
+
+}
+
+export default animPreviewsSlider;
--- app/ts/modules/full-screen/full-screen.ts
+++ app/ts/modules/full-screen/full-screen.ts
@@ -5,20 +5,17 @@ import getOptionsBreakpoints from "../../helpers/get-options-breakpoints";
 function fullScreen(pmGallery: any) {
     const body = document.querySelector('body');
     const wrapper = pmGallery.wrapper;
-    let flagFullScreen = false;
 
-    (!flagFullScreen) ? flagFullScreen = true : flagFullScreen = false;
+    (pmGallery.fullScreen) ? pmGallery.fullScreen = false : pmGallery.fullScreen = true;
 
     wrapper.classList.toggle(pmgClasses['fullScreen']);
     body.classList.toggle(pmgClasses['overflow']);
-
-    pmGallery.fullScreen = flagFullScreen;
     
     getOptionsBreakpoints(pmGallery);
     initPosPreviews(pmGallery);
 
     wrapper.dispatchEvent(new CustomEvent("fullScreen", {
-        detail: { fullScreen: flagFullScreen }
+        detail: { fullScreen: pmGallery.fullScreen }
     }));
 
 }
