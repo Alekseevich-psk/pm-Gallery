4b55749e6fc8ff0e46965d1efe870d14f418c04e
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-11-16 15:13:49 +0300

    fixed init script for gallery

--- app/scripts/pm-gallery.js
+++ app/scripts/pm-gallery.js
@@ -2,7 +2,8 @@
     const gallery = document.querySelector(".pm-gallery");
 
     new PmGallery(".pm-gallery", {
-        countPreSlides: 4.5,
+        countPreSlides: 4,
+        positionPreviews: 'bottom',
         controls: {
             btnPrev: ".main__btn-slide--prev",
             btnNext: ".main__btn-slide--next",
