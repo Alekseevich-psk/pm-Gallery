3ff6b5a9f7b9709c892af50d38dbbb6385ce0120
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-11-23 15:41:17 +0300

    fixed auto play

--- app/ts/modules/autoplay/init-auto-play.ts
+++ app/ts/modules/autoplay/init-auto-play.ts
@@ -1,10 +1,12 @@
+import pmGalleryClasses from "../../core/setting/pmgClasses";
 import autoPlay from "./auto-play";
 
 function initAutoPlay(pmGallery: any) {
     const optionAutoPlay = Number(pmGallery.initOptions.autoPlay);
     if (!optionAutoPlay) return pmGallery.btnAutoPlay.style.display = 'none';
-    
+
     pmGallery.btnAutoPlay.style.display = 'block';
+    if (pmGallery.wrapper.classList.contains(pmGalleryClasses['pause'])) return;
 
     if (pmGallery.autoPlayTimerId) {
         clearTimeout(pmGallery.autoPlayTimerId);
