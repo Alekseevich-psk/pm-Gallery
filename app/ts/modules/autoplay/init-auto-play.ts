1bfb33296cc38870ee190192b0e28f5f7d86d10b
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-12-24 23:25:42 +0300

    fixed autoplay

--- app/ts/modules/autoplay/init-auto-play.ts
+++ app/ts/modules/autoplay/init-auto-play.ts
@@ -1,16 +1,24 @@
 import autoPlay from "./auto-play";
+import pmGalleryClasses from "../../core/setting/pmgClasses";
 
 function initAutoPlay(pmGallery: any) {
+    const wrapper = pmGallery.wrapper as HTMLElement;
     const optionAutoPlay = Number(pmGallery.initOptions.autoPlay);
-    if (!optionAutoPlay) return pmGallery.btnAutoPlay.style.display = 'none';
-    
-    pmGallery.btnAutoPlay.style.display = 'block';
+
+    if (wrapper.classList.contains(pmGalleryClasses['pause'])) {
+        return clearTimeout(pmGallery.autoPlayTimerId);
+    }
+
+    if (!optionAutoPlay) {
+        return pmGallery.btnAutoPlay.style.display = 'none';
+    }
 
     if (pmGallery.autoPlayTimerId) {
         clearTimeout(pmGallery.autoPlayTimerId);
         return autoPlay(pmGallery, optionAutoPlay);
     }
 
+    pmGallery.btnAutoPlay.style.display = 'block';
     return autoPlay(pmGallery, optionAutoPlay);
 }
 
